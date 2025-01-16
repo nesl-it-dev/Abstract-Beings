c2d39d29570080c14df79d516470e8477f40bab9
nesl-it-dev <zeeshan@nesl-it.com>
2025-01-17 00:24:18 +0500

    cache fix other than vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,21 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async headers() {
+    return [
+      {
+        source:
+          "/_next/static/(.*).(js|css|json|woff2|eot|ttf|svg|png|jpg|jpeg|gif)$",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
